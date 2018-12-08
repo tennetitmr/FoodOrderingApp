ad53c477a67d15e85dc1e4d236a87a5044a098df
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-08 12:13:02 +0100

    Fixed data type mismatch bug

--- FoodOrderingApp/src/main/java/org/upgrad/repositories/UserAuthTokenRepository.java
+++ FoodOrderingApp/src/main/java/org/upgrad/repositories/UserAuthTokenRepository.java
@@ -7,16 +7,27 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.upgrad.models.UserAuthToken;
 
-// This repository interface is responsible for the interaction between the user auth token service with the user_auth_token database
+// Controller  ------> Services Layer ---> Data Access Layer (Repository)  --> Database
+//                                                                  UserAuthTokenRepository
+// This UserAuthTokenRepository interface: User Auth Token Service ------------------------> user_auth_token table
+
 
 @Repository
 public interface UserAuthTokenRepository extends CrudRepository<UserAuthToken, Integer> {
 
+    // To checks whether the user is Logged in
     @Query(nativeQuery = true,value = "SELECT * FROM USER_AUTH_TOKEN WHERE access_token=?1")
     UserAuthToken isUserLoggedIn(String accessToken);
 
+    // To Returns userId  
+    @Query(nativeQuery = true,value = "SELECT user_id FROM USER_AUTH_TOKEN WHERE access_token=?1")
+    Integer getUserId(String accessToken);
+
+    // To Remove accesstoken
     @Transactional
     @Modifying
     @Query(nativeQuery = true,value="UPDATE USER_AUTH_TOKEN SET logout_at=NOW() WHERE access_token=?1")
     void removeAuthToken( String accessToken);
+
+
 }
