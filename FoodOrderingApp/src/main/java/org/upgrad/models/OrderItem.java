c473f58c6629b34a5c5758302931793bf1e7b7bd
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-08 11:35:56 +0100

    Code merged after successful build

--- FoodOrderingApp/src/main/java/org/upgrad/models/OrderItem.java
+++ FoodOrderingApp/src/main/java/org/upgrad/models/OrderItem.java
@@ -1,6 +1,87 @@
 package org.upgrad.models;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/* Controller  ------> Services Layer ---> Data Access Layer (Model)
+ * OrderItem model: Map attributes  ----> columns in the ORDER_ITEM table in the restaurant database.
+ * Also,Contains Annotations, getters and setters. Annotations map the fields to table columns.
+ */
+
+@Entity
+@Table(name = "ORDER_ITEM")
 public class OrderItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Transient
+    @OneToOne(fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Order order;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Item item;
+
+    @Column(name = "quantity",nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price",nullable = false)
+    private Integer price;
+
+    public OrderItem(){}
+
+    public OrderItem(Order order, Item item, Integer quantity, Integer price) {
+        this.order = order;
+        this.item = item;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    //Getter and setter methods
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
