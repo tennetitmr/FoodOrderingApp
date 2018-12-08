61654f5008046a8a2843c59ebfc9f30baf014a29
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-08 12:39:11 +0100

    Update UserService.java

--- FoodOrderingApp/src/main/java/org/upgrad/services/UserService.java
+++ FoodOrderingApp/src/main/java/org/upgrad/services/UserService.java
@@ -1,17 +1,33 @@
 package org.upgrad.services;
 
 import org.upgrad.models.User;
-import org.upgrad.models.UserAuthToken;
 
 import java.util.Optional;
 
-/*
- * This UserService interface gives the list of all the service that exist in the user service implementation class.
- * Controller class will be calling the service methods by this interface.
+/*               interface
+/*  Controller ------------------> Service Methods
+ *  UserService Interface : List of all the service methods that exist in the user service implementation class.
  */
 public interface UserService {
 
+    // input:contactNumber    output: Password
     String findUserPassword(String contactNumber);
 
+    // input: id  output: User details
+    User getUserById(Integer id);
+
+    // input: contactNumber output: User Details
     User findUser(String contactNumber);
+
+    // input: id  output: User Details
+    User findUserId(Integer id);
+
+    // To save userDetails to users table.
+    void addUserDetails(User newuser);
+
+    // To updateUserDetails
+    User updateUser(String firstname, String lastname, Integer id);
+
+    // To UpdatePassword
+    Integer updateUserPassword(String password, Integer id);
 }
