0f9cf56911ddab678b3ca05b5d64e0f3b271a0eb
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-08 11:36:41 +0100

    Code merged after successful build

--- FoodOrderingApp/src/main/java/org/upgrad/models/Payment.java
+++ FoodOrderingApp/src/main/java/org/upgrad/models/Payment.java
@@ -1,5 +1,48 @@
 package org.upgrad.models;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+/* Controller  ------> Services Layer ---> Data Access Layer (Model)
+ * Address model: Map attributes  ----> columns in the PAYMENT table in the restaurant database.
+ * Also,Contains Annotations, getters and setters. Annotations map the fields to table columns.
+ */
+
+@Entity
+@Table(name = "PAYMENT")
 public class Payment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "payment_name")
+    private String paymentName;
+
+    public Payment(){}
+
+    public Payment(String paymentName) {
+        this.paymentName = paymentName;
+    }
+
+    //Getter & Setter methods
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPaymentName() {
+        return paymentName;
+    }
+
+    public void setPaymentName(String paymentName) {
+        this.paymentName = paymentName;
+    }
 }
