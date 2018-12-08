594e091a6a5c55673814dc0ce8bbdf7fec1ef72d
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-08 12:31:59 +0100

    Update PaymentService.java

--- FoodOrderingApp/src/main/java/org/upgrad/services/PaymentService.java
+++ FoodOrderingApp/src/main/java/org/upgrad/services/PaymentService.java
@@ -1,4 +1,9 @@
 package org.upgrad.services;
 
+import org.upgrad.models.Payment;
+
 public interface PaymentService {
+
+    //Get all payment methods
+    Iterable<Payment> getPaymentMethods();
 }
