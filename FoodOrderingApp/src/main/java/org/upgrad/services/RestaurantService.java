99642c1b39a46a2306f71202aebba4e61db77402
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-08 12:33:39 +0100

    Update RestaurantService.java

--- FoodOrderingApp/src/main/java/org/upgrad/services/RestaurantService.java
+++ FoodOrderingApp/src/main/java/org/upgrad/services/RestaurantService.java
@@ -3,12 +3,18 @@ package org.upgrad.services;
 import org.upgrad.models.Restaurant;
 //import org.upgrad.models.RestaurantResponse;
 import org.upgrad.requestResponseEntity.RestaurantResponse;
+import org.upgrad.requestResponseEntity.RestaurantResponseCategorySet;
+
+/*               interface
+/*  Controller ------------------> Service Methods
+ *  RestaurantService Interface : List of all the service methods that exist in the Restaurant service implementation class.
+ */
 
 public interface RestaurantService {
 
     Iterable<RestaurantResponse> getAllRestaurant();
     Iterable<RestaurantResponse> getRestaurantByName(String name);
     Iterable<RestaurantResponse> getRestaurantByCategory(String category);
-    Restaurant getRestaurantDetails(int id);
-    Restaurant updateRating(int restaurantId, Double rating);
+    RestaurantResponseCategorySet getRestaurantDetails(int id);
+    Restaurant updateRating(int restaurantId, int rating);
 }
