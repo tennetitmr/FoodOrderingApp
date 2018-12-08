9b8d91cab8518a807a8fb275e5369fd7a8e826cc
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-08 11:33:22 +0100

    Merged after successful build

--- FoodOrderingApp/src/main/java/org/upgrad/models/Coupon.java
+++ FoodOrderingApp/src/main/java/org/upgrad/models/Coupon.java
@@ -1,5 +1,58 @@
 package org.upgrad.models;
 
+import javax.persistence.*;
+
+/* Controller  ------> Services Layer ---> Data Access Layer (Model)
+ * Coupon model: Map attributes  ----> columns in the Coupon table in the restaurant database.
+ * Also,Contains Annotations, getters and setters. Annotations map the fields to table columns.
+ */
+@Entity
+@Table(name = "COUPON")
 public class Coupon {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "coupon_name")
+    private String couponName;
+
+    @Column(name = "percent",nullable = false)
+    private int percent;
+
+
+    public Coupon(){}
+
+  
+    public Coupon(String couponName, int percent) {
+        this.couponName = couponName;
+        this.percent = percent;
+    }
+
+    //Getters & Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCouponName() {
+        return couponName;
+    }
+
+    public void setCouponName(String couponName) {
+        this.couponName = couponName;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+
+    public void setPercent(int percent) {
+        this.percent = percent;
+    }
+
 }
+
