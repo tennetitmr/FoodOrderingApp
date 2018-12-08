8a30585a8f0e3e193d31b829f2dd4923ad57aa42
tennetitmr <srinivas.tenneti@gmail.com>
2018-12-08 12:32:53 +0100

    Update PaymentServiceImpl.java

--- FoodOrderingApp/src/main/java/org/upgrad/services/PaymentServiceImpl.java
+++ FoodOrderingApp/src/main/java/org/upgrad/services/PaymentServiceImpl.java
@@ -1,4 +1,27 @@
 package org.upgrad.services;
 
-public class PaymentServiceImpl{
+import org.springframework.stereotype.Service;
+import org.upgrad.models.Payment;
+import org.upgrad.repositories.PaymentRepository;
+
+import javax.transaction.Transactional;
+
+/*  Controller ---> Service --> Service Implementation
+    PaymentServiceImplementation contains Payment Service related methods.
+ */
+
+@Service
+@Transactional
+public class PaymentServiceImpl implements PaymentService{
+
+    private final PaymentRepository paymentRepository;
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    @Override
+    public Iterable<Payment> getPaymentMethods(){
+        return paymentRepository.getPaymentDetails();
+    }
 }
